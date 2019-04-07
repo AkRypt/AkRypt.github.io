d640da3056dd092969f48262f215f65965a25e54
Akshit Dayal <akshitdayal99@gmail.com>
2019-04-07 11:28:03 +0530

    Added Animations

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $("#navbar").toggleClass("scrolled", $(this).scrollTop() > 60);
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop=$(window).scrollTop();
+            if (pos<winTop+600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+    $(".skillbg").hover(function(){
+        $(this).css({"width":"+=60px","transition":"0.5s ease","border-right":"1px solid #2473f2"});
+    },
+    function(){
+        $(this).css({"width":"70px","border-right":"none"});
+    });
+});
+$(document).ready(function(){
+    // Add smooth scrolling to all links in navbar + footer link
+    $("#navbar a,#navbar h1").on('click', function(event) {
+  
+     // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+  
+      // Prevent default anchor click behavior
+      event.preventDefault();
+  
+      // Store hash
+      var hash = this.hash;
+  
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+  
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+        });
+      } // End if
+    });
+});
